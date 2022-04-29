04046956f8556d033a6196cdb2b4a2587e7ba675
lef237 <lef237k5@gmail.com>
2022-04-29 22:54:44 +0900

    2bowling

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,18 +23,17 @@ frames.each do |frame|
   count += 1
   if frame[0] == 10 && count < 10
     point += 10
-      if frames[count][0] == 10
-        point += 10 + frames[count + 1][0]
-      else
-        point += frames[count][0] + frames[count][1]
-      end
+    if frames[count][0] == 10
+      point += 10 + frames[count + 1][0]
+    else
+      point += frames[count][0] + frames[count][1]
+    end
   elsif frame.sum == 10 && count < 10
     point += 10 + frames[count][0]
   elsif count >= 10
     point += frame.sum
-  else 
+  else
     point += frame.sum
   end
 end
 puts point
-
