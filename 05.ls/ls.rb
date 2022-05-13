daca74d904e04b31277ecb99d4020402cb5ba6bf
lef237 <lef237k5@gmail.com>
2022-05-13 09:30:03 +0900

    use def in ls01 command

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,39 +3,48 @@
 
 COLUMN = 3
 
-array = []
-Dir.foreach('.') do |item|
-  next if ['.', '..'].include?(item)
+def receive_data_and_create_array
+  array = []
+  Dir.foreach('.') do |item|
+    next if ['.', '..'].include?(item)
 
-  array << item
+    array << item
+  end
+  # return array
+  array
 end
 
-array_size = array.size
-column = COLUMN
-row = (array_size.to_f / column).ceil
-two_dimensions_array = Array.new(row).map { Array.new(column, '') }
+def print_array
+  array = receive_data_and_create_array
+  array_size = array.size
+  column = COLUMN
+  row = (array_size.to_f / column).ceil
+  two_dimensions_array = Array.new(row).map { Array.new(column, '') }
 
-row_count = 0
-column_count = 0
-array.each do |x|
-  two_dimensions_array[row_count][column_count] = x
-  row_count += 1
-  if row == row_count
-    row_count = 0
-    column_count += 1
+  row_count = 0
+  column_count = 0
+  array.each do |x|
+    two_dimensions_array[row_count][column_count] = x
+    row_count += 1
+    if row == row_count
+      row_count = 0
+      column_count += 1
+    end
+  end
+
+  def length_for_longest_directory(array)
+    array.max_by(&:length).length + 2
+  end
+
+  one_dimension_array = two_dimensions_array.flatten
+  for_ljust = length_for_longest_directory(one_dimension_array)
+
+  print_count = 0
+  one_dimension_array.each do |y|
+    print y.ljust(for_ljust)
+    print_count += 1
+    print "\n" if (print_count % 3).zero?
   end
 end
 
-def length_for_longest_directory(array)
-  array.max_by(&:length).length + 2
-end
-
-one_dimension_array = two_dimensions_array.flatten
-for_ljust = length_for_longest_directory(one_dimension_array)
-
-print_count = 0
-one_dimension_array.each do |y|
-  print y.ljust(for_ljust)
-  print_count += 1
-  print "\n" if (print_count % 3).zero?
-end
+print_array
