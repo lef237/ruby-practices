27366382a0b91cc035cf8d61b0f197270d974b1b
lef237 <lef237k5@gmail.com>
2022-05-14 19:22:29 +0900

    Additional method name changes

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,38 +3,34 @@
 
 COLUMN = 3
 
-def receive_lists_and_create_array
-  array = []
+def receive_data
+  list = []
   Dir.foreach('.') do |item|
     next if ['.', '..'].include?(item)
 
-    array << item
+    list << item
   end
+  list
+end
 
+def formatted_list(list)
   column = COLUMN
-  row = (array.size.to_f / column).ceil
-  array << "" until array.size % column == 0
-  array = array.each_slice(row).to_a.transpose
-  array
+  row = (list.size.to_f / column).ceil
+  list << "" until list.size % column == 0
+  list = list.each_slice(row).to_a.transpose.flatten
+  list
 end
 
-def print_array
-  two_dimensions_array = receive_lists_and_create_array
+def print_list(list)
+  for_ljust = list.max_by(&:length).length + 2
 
-  # defでメソッド定義をするとnest状態となりrubocopに引っ掛かるため、lambdaを使いました
-  adjust_width_for_longest_directory = lambda { |array|
-    array.max_by(&:length).length + 2
-  }
-
-  one_dimension_array = two_dimensions_array.flatten
-  for_ljust = adjust_width_for_longest_directory.call(one_dimension_array)
-
-  print_count = 0
-  one_dimension_array.each do |y|
-    print y.ljust(for_ljust)
-    print_count += 1
-    print "\n" if (print_count % 3).zero?
+  list.each_with_index do |item, i|
+    print item.ljust(for_ljust)
+    i += 1
+    print "\n" if (i % COLUMN).zero?
   end
 end
 
-print_array
+list = receive_data
+list = formatted_list(list)
+print_list(list)
