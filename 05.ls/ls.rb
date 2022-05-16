abc066990f57d159bf508ba5094c49d95abbb44c
lef237 <lef237k5@gmail.com>
2022-05-16 11:02:00 +0900

    how to use of _1

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,42 +1,43 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def receive_data
-  list = []
+def get_files_in_current_directory
+  files = []
   Dir.foreach('.') do |item|
     next if ['.', '..'].include?(item)
 
-    list << item
+    files << item
   end
-  list
+  files
 end
 
-COLUMN = 3
-ROW = (receive_data.size.to_f / COLUMN).ceil
-
-def formatted_list(list)
-  list << '' until (list.size % COLUMN).zero?
-  list.each_slice(ROW).to_a.transpose
+def format_files(files)
+  files << '' until (files.size % COLUMN).zero?
+  files.each_slice(ROW).to_a.transpose
 end
 
 # 列ごとに幅を調節するためのメソッドです
-def for_ljust
-  list = receive_data.each_slice(ROW).to_a
-  for_ljust = []
-  list.each do |array|
-    for_ljust << array.max_by(&:length).length + 2
-  end
-  for_ljust
+def calc_column_width(files, i)
+  files.map {_1[i]}.max_by(&:length).length + 2
+  # files.map{|x| x[i]}でもＯＫ
 end
 
-def print_list(list)
-  list.each do |array|
+def print_files(files)
+  files.each do |array|
     array.each_with_index do |item, i|
-      print item.ljust(for_ljust[i])
+      print item.ljust(calc_column_width(files, i))
     end
     print "\n"
   end
 end
 
-list = formatted_list(receive_data)
-print_list(list)
+
+COLUMN = 3
+ROW = (get_files_in_current_directory.size.to_f / COLUMN).ceil
+
+
+formatted_files = format_files(get_files_in_current_directory)
+p formatted_files.map{_1[1]} # formatted_files.map{|x| x[1]}と同じ意味
+p formatted_files.map{_2}
+p [[1, 2], [3, 4]].map { _1}
+print_files(formatted_files)
