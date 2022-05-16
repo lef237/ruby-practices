0aa493b53b06b162a1b856a4d9bb14ab122ce061
lef237 <lef237k5@gmail.com>
2022-05-16 16:04:49 +0900

    before rubocop 05162

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -16,10 +16,8 @@ def format_files(files)
   files.each_slice(ROW).to_a.transpose
 end
 
-# 列ごとに幅を調節するためのメソッドです
 def calc_column_width(files, i)
   files.map {_1[i]}.max_by(&:length).length + 2
-  # files.map{|x| x[i]}でもＯＫ
 end
 
 def print_files(files)
@@ -31,13 +29,8 @@ def print_files(files)
   end
 end
 
-
 COLUMN = 3
 ROW = (get_files_in_current_directory.size.to_f / COLUMN).ceil
 
-
 formatted_files = format_files(get_files_in_current_directory)
-p formatted_files.map{_1[1]} # formatted_files.map{|x| x[1]}と同じ意味
-p formatted_files.map{_2}
-p [[1, 2], [3, 4]].map { _1}
 print_files(formatted_files)
