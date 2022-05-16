2338b70e260f29285a011e24e694b440b5754f93
lef237 <lef237k5@gmail.com>
2022-05-16 16:15:59 +0900

    after rubocop 0516

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_files_in_current_directory
+def files_in_current_directory
   files = []
   Dir.foreach('.') do |item|
     next if ['.', '..'].include?(item)
@@ -16,8 +16,8 @@ def format_files(files)
   files.each_slice(ROW).to_a.transpose
 end
 
-def calc_column_width(files, i)
-  files.map {_1[i]}.max_by(&:length).length + 2
+def calc_column_width(files, index)
+  files.map { _1[index] }.max_by(&:length).length + 2
 end
 
 def print_files(files)
@@ -30,7 +30,7 @@ def print_files(files)
 end
 
 COLUMN = 3
-ROW = (get_files_in_current_directory.size.to_f / COLUMN).ceil
+ROW = (files_in_current_directory.size.to_f / COLUMN).ceil
 
-formatted_files = format_files(get_files_in_current_directory)
+formatted_files = format_files(files_in_current_directory)
 print_files(formatted_files)
