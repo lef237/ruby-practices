94edf45348ece11eabf5d4c888bbb9afe0e8def6
lef237 <lef237k5@gmail.com>
2022-06-10 19:41:49 +0900

    長過ぎるメソッド定義を改善した

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,34 +1,31 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'optparse'
 
-
-
 def wc_normal(files)
-  if files != []
-    total_lines = 0
-    total_words = 0
-    total_bytes = 0
-    files.each_with_index do |file_name, index|
-      file_data = File.open(file_name).read
-      lines = file_data.count("\n")
-      words = file_data.scan(/\w+/).size
-      bytes = file_data.size
-      puts "#{lines.to_s.rjust(5)} #{words.to_s.rjust(5)} #{bytes.to_s.rjust(5)} #{file_name}"
-      total_lines += lines
-      total_words += words
-      total_bytes += bytes
-      if index == files.size - 1 and files.size > 1
-          puts "#{total_lines.to_s.rjust(5)} #{total_words.to_s.rjust(5)} #{total_bytes.to_s.rjust(5)} total"
-      end
-    end
-  else
-    stdin_data  = $stdin.read
-    lines = stdin_data.count("\n")
-    words = stdin_data.scan(/\w+/).size
-    bytes = stdin_data.size
-    puts "#{lines.to_s.rjust(5)} #{words.to_s.rjust(5)} #{bytes.to_s.rjust(5)}"
+  total_lines = 0
+  total_words = 0
+  total_bytes = 0
+  files.each_with_index do |file_name, index|
+    file_data = File.open(file_name).read
+    lines = file_data.count("\n")
+    words = file_data.scan(/\w+/).size
+    bytes = file_data.size
+    puts "#{lines.to_s.rjust(5)} #{words.to_s.rjust(5)} #{bytes.to_s.rjust(5)} #{file_name}"
+    total_lines += lines
+    total_words += words
+    total_bytes += bytes
+    puts "#{total_lines.to_s.rjust(5)} #{total_words.to_s.rjust(5)} #{total_bytes.to_s.rjust(5)} total" if (index == files.size - 1) && (files.size > 1)
   end
+end
+
+def wc_normal_stdin
+  stdin_data = $stdin.read
+  lines = stdin_data.count("\n")
+  words = stdin_data.scan(/\w+/).size
+  bytes = stdin_data.size
+  puts "#{lines.to_s.rjust(5)} #{words.to_s.rjust(5)} #{bytes.to_s.rjust(5)}"
 end
 
 def wc_l_option(files)
@@ -39,26 +36,28 @@ def wc_l_option(files)
       lines = file_data.count("\n")
       puts "#{lines.to_s.rjust(5)} #{file_name}"
       total_lines += lines
-      if index == files.size - 1 and files.size > 1
-          puts "#{total_lines.to_s.rjust(5)} total"
-      end
+      puts "#{total_lines.to_s.rjust(5)} total" if (index == files.size - 1) && (files.size > 1)
     end
   else
-    stdin_data  = $stdin.read
+    stdin_data = $stdin.read
     lines = stdin_data.count("\n")
-    puts "#{lines.to_s.rjust(5)}"
+    puts lines.to_s.rjust(5).to_s
   end
 end
 
-opt = OptionParser.new
-params = {}
-opt.on('-l') {|v| params[:l] = v }
-opt.parse!(ARGV)
-files = ARGV
-
-if params[:l]
-  wc_l_option(files)
-else
-  wc_normal(files)
+def main
+  opt = OptionParser.new
+  params = {}
+  opt.on('-l') { |v| params[:l] = v }
+  opt.parse!(ARGV)
+  files = ARGV
+  if params[:l]
+    wc_l_option(files)
+  elsif files != []
+    wc_normal(files)
+  else
+    wc_normal_stdin
+  end
 end
 
+main
