ffc27ee075dd7618ba348e49bc35de96f54a5f73
lef237 <lef237k5@gmail.com>
2022-10-22 21:21:50 +0900

    テストコードのchompが要らなかったので修正した。

--- 08.ls_object/test/test_object_ls.rb
+++ 08.ls_object/test/test_object_ls.rb
@@ -8,12 +8,11 @@ class TestObjectLs < Test::Unit::TestCase
   TARGET_PATHNAME = 'test/sample_files'
 
   def test_object_ls
-    expected = <<~TEXT.chomp
-      a       e    i#{'     '}
-      bbbbbb  f    jjjj#{'  '}
-      c       ggg#{'        '}
-      d       h#{'          '}
-
+    expected = <<~TEXT
+      a       e    i     
+      bbbbbb  f    jjjj  
+      c       ggg        
+      d       h          
     TEXT
     options = {}
     files_collection = FilesCollection.new(options, TARGET_PATHNAME)
@@ -21,13 +20,12 @@ class TestObjectLs < Test::Unit::TestCase
   end
 
   def test_object_ls_all_reverse
-    expected = <<~TEXT.chomp
-      jjjj  e       .k#{'  '}
-      i     d       ..#{'  '}
-      h     c       .#{'   '}
-      ggg   bbbbbb#{'      '}
-      f     a#{'           '}
-
+    expected = <<~TEXT
+      jjjj  e       .k  
+      i     d       ..  
+      h     c       .   
+      ggg   bbbbbb      
+      f     a           
     TEXT
     options = { all: true, reverse: true }
     files_collection = FilesCollection.new(options, TARGET_PATHNAME)
