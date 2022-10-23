04e11b4b1528c3ac7545c949df1b204fd897b780
lef237 <lef237k5@gmail.com>
2022-10-23 18:07:46 +0900

    相対パスを修正した。

--- 08.ls_object/lib/list.rb
+++ 08.ls_object/lib/list.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative './file'
-require_relative './format'
+require_relative 'file'
+require_relative 'format'
 
 class List
   def initialize(options, pathname)
